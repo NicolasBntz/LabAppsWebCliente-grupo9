85f75f052760dd243ee6a275a39a0897f191d52f
Nicolás Benitez <nicolas.benitez@istea.com.ar>
2026-04-29 21:17:24 -0300

    se implementa generación dinámica de cards de productos a partir de API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import { getProducts } from "./api.js";
 
+let productList = document.querySelector('#product-list')
+
 getProducts().then((products) => {
-  console.log(products);
+    let template= ' '; 
+    products.forEach(p => {
+        template += `
+            <div class="col">
+                <div class="card">  
+                    <img src="${p.image}" class="card-img-top" alt="${p.title}">
+                    <div class="card-body">
+                         <h5 class="card-title">${p.title}</h5>
+                         <p class="card-text">${p.description}</p>
+                    </div>
+                </div>
+            </div>
+        `;    
+    });
+
+    productList.innerHTML = template;
 });
+
