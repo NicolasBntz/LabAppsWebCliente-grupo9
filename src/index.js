64880f2b895f1e9872ca01522cd797c14148a5aa
Nicolás Benitez <nicolas.benitez@istea.com.ar>
2026-05-06 14:10:58 -0300

    Se agregan categorías y se filtra por cada una de ellas a través de un evento onclick

--- src/index.js
+++ src/index.js
@@ -6,6 +6,36 @@ import { getProducts } from "./services/api.js";
 initLocalStorage();
 getProducts().then((products) => {
         let inputSearch = document.querySelector('#inputSearch');
+        
+        let home= document.querySelector('#home');
+        home.addEventListener('click', () => {
+            return RenderCards(products);
+        });
+
+        let electronics = document.querySelector('#electronics');
+        electronics.addEventListener('click', () => {
+            let result = products.filter((p) => p.category == 'electronics');
+            return RenderCards(result);
+        });
+
+        let jewelery = document.querySelector('#jewelery');
+        jewelery.addEventListener('click', () => {
+            let result = products.filter((p) => p.category == 'jewelery');
+            return RenderCards(result);
+        });
+
+        let mens_clothing = document.querySelector('#mens-clothing');
+        mens_clothing.addEventListener('click', () => {
+            let result = products.filter((p) => p.category == 'mens-clothing');
+            return RenderCards(result);
+        });
+
+        let womens_clothing = document.querySelector('#womens-clothing');
+        womens_clothing.addEventListener('click', () => {
+            let result = products.filter((p) => p.category == 'womens-clothing');
+            return RenderCards(result);
+        });
+
         inputSearch.addEventListener('input', (event) => {
             console.log(event.target.value);
             let query = event.target.value;
